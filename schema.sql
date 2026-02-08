3a324c61b5a6b40f801e1e8707f5319c58954bde
Delna-Maria <delnaj895@gmail.com>
2026-02-08 17:44:12 +0530

    Added MedDiary database schema

---
+++ schema.sql
@@ -0,0 +1,163 @@
+-- MySQL dump 10.13  Distrib 8.0.44, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: meddiary
+-- ------------------------------------------------------
+-- Server version	8.0.44
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `ddi_history`
+--
+
+DROP TABLE IF EXISTS `ddi_history`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ddi_history` (
+  `ddi_id` int NOT NULL AUTO_INCREMENT,
+  `member_id` int NOT NULL,
+  `drug1` varchar(100) DEFAULT NULL,
+  `drug2` varchar(100) DEFAULT NULL,
+  `severity` varchar(20) DEFAULT NULL,
+  `message` text,
+  `checked_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`ddi_id`),
+  KEY `member_id` (`member_id`),
+  CONSTRAINT `ddi_history_ibfk_1` FOREIGN KEY (`member_id`) REFERENCES `family_members` (`member_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `emergency_contacts`
+--
+
+DROP TABLE IF EXISTS `emergency_contacts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `emergency_contacts` (
+  `contact_id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `emergency_phone` varchar(15) NOT NULL,
+  `relationship` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`contact_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `emergency_contacts_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `family_members`
+--
+
+DROP TABLE IF EXISTS `family_members`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `family_members` (
+  `member_id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `age` int DEFAULT NULL,
+  `gender` varchar(20) DEFAULT NULL,
+  `blood_group` varchar(10) DEFAULT NULL,
+  `allergies` text,
+  PRIMARY KEY (`member_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `family_members_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `medictaions`
+--
+
+DROP TABLE IF EXISTS `medictaions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `medictaions` (
+  `med_id` int NOT NULL AUTO_INCREMENT,
+  `member_id` int NOT NULL,
+  `med_name` varchar(100) DEFAULT NULL,
+  `dosage` varchar(50) DEFAULT NULL,
+  `frequency` varchar(50) DEFAULT NULL,
+  `start_date` date DEFAULT NULL,
+  `end_date` date DEFAULT NULL,
+  PRIMARY KEY (`med_id`),
+  KEY `member_id` (`member_id`),
+  CONSTRAINT `medictaions_ibfk_1` FOREIGN KEY (`member_id`) REFERENCES `family_members` (`member_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `otp_verification`
+--
+
+DROP TABLE IF EXISTS `otp_verification`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `otp_verification` (
+  `phone_number` varchar(15) NOT NULL,
+  `otp` varchar(6) DEFAULT NULL,
+  `expires_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`phone_number`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `test_results`
+--
+
+DROP TABLE IF EXISTS `test_results`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `test_results` (
+  `test_id` int NOT NULL AUTO_INCREMENT,
+  `member_id` int NOT NULL,
+  `test_name` varchar(100) DEFAULT NULL,
+  `value` float DEFAULT NULL,
+  `unit` varchar(20) DEFAULT NULL,
+  `normal_min` float DEFAULT NULL,
+  `normal_max` float DEFAULT NULL,
+  `status` varchar(20) DEFAULT NULL,
+  `test_date` date DEFAULT NULL,
+  PRIMARY KEY (`test_id`),
+  KEY `member_id` (`member_id`),
+  CONSTRAINT `test_results_ibfk_1` FOREIGN KEY (`member_id`) REFERENCES `family_members` (`member_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `user_id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) DEFAULT NULL,
+  `phone_number` varchar(15) NOT NULL,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `phone` (`phone_number`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-02-08 17:30:26
