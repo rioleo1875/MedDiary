a12a9641abacc983ce51680154d16284c91df70a
rioleo1875 <diyashyam1875@gmail.com>
2026-02-10 21:05:33 +0530

    db.js fix

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,18 +1,10 @@
-<<<<<<< Updated upstream
-const mysql = require("mysql2");
-=======
 const mysql = require("mysql2/promise");
->>>>>>> Stashed changes
-require("dotenv").config();
 
 const db = mysql.createPool({
   host: "localhost",
   user: "meddiary_user",
   password: "meddiary123",
-  database: "meddiary",
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
+  database: "meddiary"
 });
 
 module.exports = db;
