2dbef3be07ba3a179b0d67a14cee2e65db383c6a
Delna-Maria <delnaj895@gmail.com>
2026-03-25 02:29:29 +0530

    db.js updateee

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,11 @@
 const mysql = require("mysql2/promise");
 
 const db = mysql.createPool({
-  host: "localhost",
-  user: "meddiary_user",
-  password: "meddiary123",
-  database: "meddiary"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
 module.exports = db;
