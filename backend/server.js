5f23821092321d8c3c045513e27b8dc2d7c0c203
Delna-Maria <delnaj895@gmail.com>
2026-03-25 17:03:13 +0530

    changes againn

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,3 @@
-console.log("1. Starting server.js");
-require("dotenv").config();
-console.log("2. Dotenv loaded");
-
-const app = require("./app");
-console.log("3. App loaded");
-
-const PORT = process.env.PORT || 3000;
-console.log("4. PORT:", PORT);
-
-process.on("uncaughtException", (err) => {
-  console.error("Uncaught Error:", err);
-  console.error(err.stack);
-});
-
-console.log("5. About to listen...");
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-console.log("6. Listen called");
 
 require("dotenv").config();
 
