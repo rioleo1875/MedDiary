7eae57fce7b89984251e53e7caf7abab6fae272b
Delna-Maria <delnaj895@gmail.com>
2026-03-25 23:01:50 +0530

    IPv4

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -12,6 +12,7 @@ if (process.env.EMAIL_USER && process.env.EMAIL_PASS) {
     host: "smtp.gmail.com",
     port: 587,
     secure: false, // TLS
+    family: 4, // Force IPv4
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
