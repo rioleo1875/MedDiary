2d68e2397eddbad987e0a4b7009abf90b037e4bd
Delna-Maria <delnaj895@gmail.com>
2026-03-25 16:56:43 +0530

    changeess

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,18 @@
+console.log("=== SERVER.JS IS RUNNING ===");
 require("dotenv").config();
 
 const app = require("./app");
 
 const PORT = process.env.PORT || 3000;
 
+console.log("PORT:", PORT);
+
 process.on("uncaughtException", (err) => {
   console.error("Uncaught Error:", err);
+  console.error(err.stack);
 });
 
+console.log("Starting server...");
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
