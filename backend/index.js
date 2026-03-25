cc606572d267b07be7f3e19d28162afe117e241e
Delna-Maria <delnaj895@gmail.com>
2026-03-25 14:55:51 +0530

    index.js

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+
+const app = require("./app");
+
+const PORT = process.env.PORT || 3000;
+
+process.on("uncaughtException", (err) => {
+  console.error("Uncaught Error:", err);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
